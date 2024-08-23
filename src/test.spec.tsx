b011b5e92a2cc8fe22e5a5bab5a34a286c628d18
Km Pooja <kpooja@atlassian.com>
2024-08-23 14:50:25 +0530

    repro memory issue in playwright 1.42

--- src/test.spec.tsx
+++ src/test.spec.tsx
@@ -1,11 +1,7 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from "@playwright/experimental-ct-react";
+import App from "./App.tsx";
 
-test.describe("Example Test Suite", () => {
-  test("should have the correct page title", async ({ page }) => {
-    // Navigate to the website
-    await page.goto("https://example.com");
-
-    // Expect the title to be "Example Domain"
-    await expect(page).toHaveTitle("Example Domain");
-  });
+test("should work", async ({ mount }) => {
+  const component = await mount(<App />);
+  await expect(component).toContainText("Learn React");
 });
